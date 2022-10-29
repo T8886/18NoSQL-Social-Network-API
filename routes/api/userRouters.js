5d166bbdcd8c69c749d3c15f1efb9e7d2085e25b
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-10-29 13:38:59 -0500

    working on controllers

--- routes/api/userRouters.js
+++ routes/api/userRouters.js
@@ -8,19 +8,22 @@ const {
     deleteUser,
     addFriend,
     deleteFriend
-} = require('../../controllers/userController');
+} = require('../../controllers/userController.js');
 
 // /api/users GET all and POST 
-router.route('/').get(getUser).post(createUser);
+router
+.route('/').get(getUser).post(createUser);
 
 // /api/users/:userId GET one user, PUT and DELETE by user's ID
-router.route('/:userId')
+router
+.route('/:userId')
 .get(getSingleUser)
 .put(updateUser)
 .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
-router.route('/:userId/friends/:friendId')
+router
+.route('/:userId/friends/:friendId')
 .post(addFriend)
 .delete(deleteFriend);
 
